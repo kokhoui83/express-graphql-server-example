5e4ab7a1638342dd548afd4c0fcb7d11bbd3dd56
brian lim <kokhoui.lim@duerrdental.com>
2021-04-08 12:12:45 +0800

    add mutation createFriend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
+import crypto from 'crypto'
 import schema from './schema.js'
 
 const app = express()
@@ -8,8 +9,35 @@ app.get('/', (req, res) => {
     res.send('GraphQL is amazing')
 })
 
+class Friend {
+    constructor (id, { firstName, lastName, gender, language, email }) {
+        this.id = id
+        this.firstName = firstName
+        this.lastName = lastName
+        this.gender = gender
+        this.language = language
+        this.email = email
+    }
+}
+
+const friendDB = {}
+
 const root = {
-    hello: () => "Hi, I'm Brian!"
+    friend: () => {
+        return {
+            id: 123456,
+            firstName: 'John',
+            lastName: 'Doe',
+            gender: 'male',
+            language: 'en-US',
+            email: 'john.doe@example.com'
+        }
+    },
+    createFriend: ({input}) => {
+        const id = crypto.randomBytes(10).toString('hex')
+        friendDB[id] = input
+        return new Friend(id, input)
+    }
 }
 
 app.use('/graphql', graphqlHTTP({
