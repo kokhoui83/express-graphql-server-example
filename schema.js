28c9ef49c96f8f42a8067a2189e412490c8826de
brian lim <kokhoui.lim@duerrdental.com>
2021-04-08 13:55:44 +0800

    add gender to schema - using enum

--- schema.js
+++ schema.js
@@ -5,9 +5,16 @@ const schema = buildSchema(`
         id: ID
         firstName: String
         lastName: String
-        gender: String
+        gender: Gender
+        age: Int
         language: String
         email: String
+    }
+
+    enum Gender {
+        MALE
+        FEMALE
+        OTHER
     }
 
     type Query {
@@ -18,7 +25,8 @@ const schema = buildSchema(`
         id: ID
         firstName: String!
         lastName: String
-        gender: String
+        gender: Gender
+        age: Int
         language: String
         email: String
     }
